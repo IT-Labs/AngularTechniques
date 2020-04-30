00bf77d87da94afd79b6297768fe378df880ac20
Kiril <kiril_zafirov@yahoo.com>
2020-04-30 10:05:27 +0200

    adding additional param to impure pipe

--- directives-and-pipes/src/app/pipes/custom/impure.pipe.ts
+++ directives-and-pipes/src/app/pipes/custom/impure.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'impureList'
+  name: 'impureList',
+  pure:false
 })
 export class ImpurePipe implements PipeTransform {
 
-  transform(list: any[], args?: any): any {
-    return list.filter(itemInList => itemInList[args]);
+  transform(list: any[], args: any , flag): any[] {
+    return list.filter(itemInList => itemInList[args] === flag);
   }
 
 }
